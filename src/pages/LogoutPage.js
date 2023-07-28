eef16860245322cc08e3a4c2831792736f1775bd
YYYOOYY <dideoduf1@gmail.com>
2023-07-28 17:13:07 +0900

    full commit

--- src/pages/LogoutPage.js
+++ src/pages/LogoutPage.js
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
-import { jwtState } from "..";
+import { jwtState, loginIdState } from "..";
 import { useNavigate } from "react-router-dom";
 
 function LogoutPage() {
     const [jwt, setJwt] = useRecoilState(jwtState);
+    const [loginId, setLoginId] = useRecoilState(loginIdState);
     const navigate = useNavigate();
     useEffect(() => {
         if(jwt){
-            setJwt(null)
+            setJwt(null);
+            setLoginId(null);
             sessionStorage.removeItem("authLoginId");
             sessionStorage.removeItem("authToken");
             navigate("/")
